2091a235038b0daf1ab8a5ea640875eb6798c721
Preritkushwaha <152644623+Preritkushwaha@users.noreply.github.com>
2024-11-12 17:39:06 +0000

    done

--- frontend/CodeCrafters/Components/Games/Card.jsx
+++ frontend/CodeCrafters/Components/Games/Card.jsx
@@ -1,19 +1,23 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
 
-function Card({ imageSrc, title, subtitle }) {
- 
+function Card({ imageSrc, title, subtitle, ondip }) {
+  const [isActive, setIsActive] = useState(false);
 
- 
+  const activateCard = () => {
+    setIsActive(!isActive);
+    ondip();
+  };
+
   return (
-    <div className="max-w-sm rounded-xl overflow-hidden shadow-lg cursor-pointer  bg-white" >
-      {" "}
-      <img className="w-full" src={imageSrc} alt="Card Image" />{" "}
-      <div className="px-6 py-4">
-        {" "}
-        <div className="font-bold text-xl mb-2">{title}</div>{" "}
-        <p className="text-gray-700 text-base">{subtitle}</p>{" "}
-      </div>{" "}
+    <div
+      className={`max-w-sm rounded-xl overflow-hidden h-80 w-56 shadow-lg cursor-pointer ${!isActive ? 'bg-white duration-150 ease-in-out' : 'bg-green-100 border-2 border-solid border-green-800 ease-in-out scale-90 duration-200'}`}
+      onClick={activateCard}
+    >
+      <img className="w-full" src={imageSrc} alt="Card Image" />
+      <div className="px-6 py-2">
+        <div className="font-bold text-xl mb-2">{title}</div>
+        <p className="text-gray-700 text-base">{subtitle}</p>
+      </div>
     </div>
   );
 }
--- frontend/CodeCrafters/Components/Header/Header.jsx
+++ frontend/CodeCrafters/Components/Header/Header.jsx
@@ -1,24 +1,65 @@
-import React from 'react'
-import {Link, NavLink} from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-function Header() {
-
+const Header = () => {
   return (
     <>
-    <header className='bg-matte-green fixed z-10 h-20 w-full flex items-center '>
-      <img src="https://tse3.mm.bing.net/th?id=OIG3.36IQ8o9iRRiFTOS4S0Ww&pid=ImgGn" className='h-16 rounded-lg ml-5' alt="" />
-      <h1 className='text-3xl  text-yellow-500  text-border ml-1 font-bold'>Mahila</h1>
-      <h1 className='text-3xl text-cyan-600 text-border ml-1 font-bold'>Mitra</h1>
-      <nav className='ml-auto flex gap-12 mr-9 text-lg text-teal-950 font-semibold  '>
-       <NavLink to={""} className='text-white' >Home</NavLink>
-       <NavLink to={"games"} className='text-white' >Services</NavLink>
-       <NavLink to={"medical"} className='text-white' >Medical</NavLink>
-
-        </nav>
-
-    </header>
+      {" "}
+      <header className="bg-matte-green fixed z-10 h-20 w-full flex items-center px-4 md:px-8">
+        {" "}
+        <img
+          src="https://tse3.mm.bing.net/th?id=OIG3.36IQ8o9iRRiFTOS4S0Ww&pid=ImgGn"
+          className="h-12 md:h-16 rounded-lg"
+          alt=""
+        />{" "}
+        <div className="flex flex-col md:flex-row ml-2 md:ml-5">
+          {" "}
+          <h1 className="text-2xl md:text-3xl text-yellow-500 text-border font-bold">
+            Code
+          </h1>{" "}
+          <h1 className="text-2xl md:text-3xl text-cyan-600 text-border font-bold">
+            Crafters
+          </h1>{" "}
+        </div>{" "}
+        <nav className="ml-auto flex gap-4 md:gap-12 text-sm md:text-lg text-teal-950 font-semibold">
+          {" "}
+          <NavLink
+            to=""
+            className={({ isActive }) =>
+              `${
+                isActive ? "text-yellow-400" : "text-black lg:text-white md:text-white "
+              }`
+            }
+          >
+            {" "}
+            Home{" "}
+          </NavLink>{" "}
+          <NavLink
+            to="games"
+            className={({ isActive }) =>
+              `${
+                isActive ? "text-yellow-400" : "text-black lg:text-white md:text-white "
+              }`
+            }
+          >
+            {" "}
+            Services{" "}
+          </NavLink>{" "}
+          <NavLink
+            to="medical"
+            className={({ isActive }) =>
+              `${
+                isActive ? "text-yellow-400" : "text-black lg:text-white md:text-white "
+              }`
+            }
+          >
+            {" "}
+            Medical{" "}
+          </NavLink>{" "}
+        </nav>{" "}
+      </header>{" "}
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
